c7d91bfce227485cc8e3129e7e512ea4091c576e
zemaetibark <114019206+zemaetibark@users.noreply.github.com>
2023-01-13 15:52:08 +0100

    Update automailer.go

--- automailer.go
+++ automailer.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	
 	gomail "gopkg.in/gomail.v2"
 )
 
@@ -12,7 +12,7 @@ func main() {
 	m.SetHeader("From", "applicatieopdracht@gmail.com")
 	m.SetHeader("To", "zemaetibark@hotmail.com")
 	m.SetHeader("Subject", "Even testen voor applicatie ;)")
-	m.SetBody("text/plain", "de body testen")
+	m.SetBody("text/plain", "De body testen")
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "applicatieopdracht@gmail.com", "wgftnptcxmownaus")
 
