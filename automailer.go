98b675952ef22a277818cfe2c1da6428697a29f0
zemaetibark <114019206+zemaetibark@users.noreply.github.com>
2023-01-13 15:49:36 +0100

    Add files via upload
    Automailer 1.0

---
+++ automailer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	gomail "gopkg.in/gomail.v2"
+)
+
+func main() {
+	m := gomail.NewMessage()
+
+	m.SetHeader("From", "applicatieopdracht@gmail.com")
+	m.SetHeader("To", "zemaetibark@hotmail.com")
+	m.SetHeader("Subject", "Even testen voor applicatie ;)")
+	m.SetBody("text/plain", "de body testen")
+
+	d := gomail.NewDialer("smtp.gmail.com", 587, "applicatieopdracht@gmail.com", "wgftnptcxmownaus")
+
+	d.DialAndSend(m)
+	fmt.Println("mail sent!")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
